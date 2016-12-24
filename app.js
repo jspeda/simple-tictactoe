3575f95e7e31f50c16b3d7a44f8b676da7e51c4b
Joseph Spedale <jspedale@gmail.com>
2016-12-23 22:59:03 -0500

    X shows up in the correct box when clicked and logs cell ID

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 $(document).ready(function() {
 
   GameBoard = {
-    this.gameArray: [0,0,0,0,0,0,0,0,0],
-    this.gameLabels: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    init: function() {
+      this.gameArray = [0,0,0,0,0,0,0,0,0];
+      this.gameLabels = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    },
 
     makeBoard: function() {
       // write a function to link the gameArray to the cells on the page
     }
-  }
-
-  Actions = Object.create(GameBoard);
-
-  Actions.fillSquare = function() {
-    $('.square').click(function() {
-      if((this).html() === "") {
-        (this).html("X");
-        // need to mark off that specific box in the array.
-      }
-    });
   };
 
-  Actions.computerFill = function() {
+  Game = Object.create(GameBoard);
+
+  Game.init();
+
+  Game.fillSquare = function(item) {
+      if(item.html() === "") {
+        item.html("X");
+        // need to mark off that specific box in the array.
+      };
+  };
+
+  Game.computerFill = function() {
     // write a function to have the computer choose its square.
   };
-}
+
+$('.cell').click(function() {
+  var that = $(this);
+  Game.fillSquare(that);
+  console.log(that.attr('id'));
+});
+
 
 });
