7e613a097a4bfc01bab4591e21a87780fe43bb3d
Joseph Spedale <jspedale@gmail.com>
2016-12-23 23:46:49 -0500

    this.gameArray now changes according to cell selected by user

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ $(document).ready(function() {
     init: function() {
       this.gameArray = [0,0,0,0,0,0,0,0,0];
       this.gameLabels = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-    },
-
-    makeBoard: function() {
-      // write a function to link the gameArray to the cells on the page
     }
+
+    // list out all winning combinations
   };
 
   Game = Object.create(GameBoard);
@@ -16,20 +14,26 @@ $(document).ready(function() {
   Game.init();
 
   Game.fillSquare = function(item) {
-      if(item.html() === "") {
-        item.html("X");
-        // need to mark off that specific box in the array.
-      };
+    if(item.html() === "") {
+      item.html("X");
+      var currentSpot = this.gameLabels.indexOf(item.attr('id'));
+      console.log(currentSpot);
+      console.log(item.attr('id'));
+      this.gameArray[currentSpot] = 1;
+      console.log(this.gameArray);
+    };
   };
 
   Game.computerFill = function() {
     // write a function to have the computer choose its square.
   };
 
+  // write a function to check winning combinations every turn
+
 $('.cell').click(function() {
   var that = $(this);
   Game.fillSquare(that);
-  console.log(that.attr('id'));
+  // console.log(that.attr('id'));
 });
 
 
